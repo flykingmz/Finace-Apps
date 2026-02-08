5023a97647010ed8f80adc8e7689da9d494ba713
flyking <flykingmz@gmail.com>
2026-02-08 03:40:55 +0000

    bugfix

--- finance-app/src/footlink/PrivacyPolicy.vue
+++ finance-app/src/footlink/PrivacyPolicy.vue
@@ -3,7 +3,7 @@
     <!-- 页面标题 -->
     <div class="page-header">
       <h1 class="page-title">Privacy Policy</h1>
-      <div class="last-updated">Effective Date: March 2025</div>
+      <div class="last-updated">Effective Date: 2025</div>
     </div>
 
     <!-- 主要内容区域 -->
