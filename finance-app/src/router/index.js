dd754e90ff8b522e6f670a4395a84ab55e4d9df4
flyking <flykingmz@gmail.com>
2026-02-28 12:03:42 +0800

    Update index.js

--- finance-app/src/router/index.js
+++ finance-app/src/router/index.js
@@ -120,12 +120,7 @@ const router = createRouter({
 
 // 添加全局路由守卫
 router.beforeEach((to, from, next) => {
-  if (window.location.hash) {
-    const path = window.location.hash.replace('#', '')
-    next(path)
-  } else {
-    next()
-  }
+  next()
 })
 
 // 添加路由解析守卫
